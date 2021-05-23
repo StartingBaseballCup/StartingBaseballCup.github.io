e5babab02edb688291032e26817a1626ee5acbf9
50933138 <50933138@gm.nfu.edu.tw>
2021-05-23 19:35:22 +0800

    06

--- init.py
+++ init.py
@@ -10,7 +10,7 @@ config_dir = _curdir + "/config/"
 class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
-    site_title = "50933138"
+    site_title = "先發盃"
     # for IPv4
     ip = "127.0.0.1"
     # for IPv6
